07506bf5b0854a6be71b9fe5bef8e334cec31505
Oscar Chiriboga <oscarmchiriboga@gmail.com>
2020-09-11 16:08:15 -0400

    added .gitignore, removed node_modules, added .env

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
-// routes
-//app.use(require("./routes/api.js"));
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
